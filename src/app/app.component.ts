e7a69b0a0188f5220fdfd75c85d963da10089389
jimena <jimenahernando@hotmail.com>
2021-08-24 10:54:25 +0200

    Componentes appComponent y home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'guards';
+export class AppComponent implements OnInit {
+  
+  login: boolean = false;
+  id: number;
+
+  constructor(){
+    this.id = 0;
+  }
+
+  ngOnInit(): void {
+  }
+  
+  ngDoCheck(): void{
+    this.login = (localStorage.getItem('logueado') ? true : false );
+    this.id = Number(localStorage.getItem('logueado'));
+  }
 }
+ 
